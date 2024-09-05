1253213752e79d681c1d0060d5e1f0186b716e97
BryanZhang <bryan.zhang@hairobotics.com>
2024-09-05 09:57:40 +0800

    start differential testing

--- src/cpu/difftest/ref.c
+++ src/cpu/difftest/ref.c
@@ -18,14 +18,19 @@
 #include <difftest-def.h>
 #include <memory/paddr.h>
 
+// 在DUT host memory的`buf`和REF guest memory的`addr`之间拷贝`n`字节,
+// `direction`指定拷贝的方向, `DIFFTEST_TO_DUT`表示往DUT拷贝, `DIFFTEST_TO_REF`表示往REF拷贝
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
   assert(0);
 }
 
+// `direction`为`DIFFTEST_TO_DUT`时, 获取REF的寄存器状态到`dut`;
+// `direction`为`DIFFTEST_TO_REF`时, 设置REF的寄存器状态为`dut`;
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
   assert(0);
 }
 
+// 让REF执行`n`条指令
 __EXPORT void difftest_exec(uint64_t n) {
   assert(0);
 }
@@ -34,6 +39,7 @@ __EXPORT void difftest_raise_intr(word_t NO) {
   assert(0);
 }
 
+// 初始化REF的DiffTest功能
 __EXPORT void difftest_init(int port) {
   void init_mem();
   init_mem();
